1f725553ad0ae4f720729f894e20ae55cb927815
Michael Voříšek <mvorisek@mvorisek.cz>
2021-05-14 21:00:42 +0200

    Delete and replace repro/PHP with official binaries
    source: https://windows.php.net/downloads/releases/php-7.4.19-nts-Win32-vc15-x64.zip

--- repro/PHP/composer.bat
+++
@@ -1,4 +0,0 @@
-@echo OFF
-setlocal DISABLEDELAYEDEXPANSION
-
-php -d memory_limit=3G -d disable_functions="" -d allow_url_fopen=on -f "%~dp0composer.phar" -- %*
--- repro/PHP/msmtp-php-sync.bat
+++
@@ -1,4 +0,0 @@
-@echo OFF
-setlocal DISABLEDELAYEDEXPANSION
-
-"C:\Program Files\PHP\msmtp.exe" %* -C "C:\Program Files\PHP\msmtprc" -t -f "no-reply@unknown-sender.com" -F """"Unknown Sender""""
